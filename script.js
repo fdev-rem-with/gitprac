5a5ca0599bca6529e2baa789d47c575a401e5b1b
fdev-rem-with <fdev.rem92@gmail.com>
2021-06-24 18:02:00 +0900

    test3

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const name = (str) => {
   console.log(`Hello ! ${str}, nice to meet you!`);
 };
 
-<<<<<<< HEAD
 // 커밋룰루
 // 커밋
 // 커밋룰루
@@ -10,10 +9,4 @@ const name = (str) => {
 // 커밋룰루
 // 커밋
 // 커밋룰루
-=======
-// 커밋
-// 커밋
-// 커밋
-// 커밋
->>>>>>> refs/remotes/origin/master
 // 커밋
